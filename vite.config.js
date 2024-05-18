cf301fecb9db79ec178783031a1209bd2cba25aa
Shamim <s.ahsan.ds96@gmail.com>
2024-05-18 10:55:11 +0600

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://task-manager-backend-bay.vercel.app",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://task-manager-backend-bay.vercel.app",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
